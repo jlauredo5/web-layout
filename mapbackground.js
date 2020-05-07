422637f3f54b01e29c8827f2a8d62d0005369944
jlauredo5 <jlauredo5@gmail.com>
2020-05-07 10:27:39 -0400

    update 1

--- mapbackground.js
+++ mapbackground.js
@@ -1,6 +1,19 @@
-const el = document.querySelector("#module");
 
-el.addEventListener("mousemove", (e) => {
-  el.style.setProperty('--x', -e.offsetX + "px");
-  el.style.setProperty('--y', -e.offsetY + "px");
-});
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 55.875582, lng: -24.877080},
+          zoom: 3
+        });
+      }
+
+      
+
+
+
+// const el = document.querySelector("#module");
+
+// el.addEventListener("mousemove", (e) => {
+//   el.style.setProperty('--x', -e.offsetX + "px");
+//   el.style.setProperty('--y', -e.offsetY + "px");
+// });
