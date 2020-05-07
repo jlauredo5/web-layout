d3e0f53118a99fff1fa23e9c18b7321438524c8c
jlauredo5 <jlauredo5@gmail.com>
2020-05-07 10:51:27 -0400

    navbar update

--- mapbackground.js
+++ mapbackground.js
@@ -1,19 +1,19 @@
 
-var map;
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 55.875582, lng: -24.877080},
-          zoom: 3
-        });
-      }
+// var map;
+// function initMap() {
+// 	map = new google.maps.Map(document.getElementById('map'), {
+//           center: {lat: 55.875582, lng: -24.877080},
+//           zoom: 3
+//         });
+//       }
 
       
 
 
 
-// const el = document.querySelector("#module");
+const el = document.querySelector("#module");
 
-// el.addEventListener("mousemove", (e) => {
-//   el.style.setProperty('--x', -e.offsetX + "px");
-//   el.style.setProperty('--y', -e.offsetY + "px");
-// });
+el.addEventListener("mousemove", (e) => {
+  el.style.setProperty('--x', -e.offsetX + "px");
+  el.style.setProperty('--y', -e.offsetY + "px");
+});
